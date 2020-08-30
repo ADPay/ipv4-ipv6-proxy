a2f903987e52588482b79deed6b9eaf194f53eda
ADPay Tech <70116946+ADPay@users.noreply.github.com>
2020-08-31 00:50:06 +0300

    Update 3proxy.sh

--- scripts/3proxy.sh
+++ scripts/3proxy.sh
@@ -41,8 +41,8 @@ EOF
 upload_proxy() {
     local PASS=$(random)
     zip --password $PASS proxy.zip proxy.txt
-    URL=$(curl -s --upload-file proxy.zip https://transfer.sh/proxy.zip)
-
+    URL=$(curl --upload-file https://transfer.sh/proxy.zip)
+    
     echo "Proxy is ready! Format IP:PORT:LOGIN:PASS"
     echo "Download zip archive from: ${URL}"
     echo "Password: ${PASS}"
